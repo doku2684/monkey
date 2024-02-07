2f54f6c4add008009c7b108ee1f9d136bf76f87f
doku2684 <kimjoon2014@gmail.com>
2024-02-07 23:11:39 +0900

    complete chapter 3.5 Prefix Expressions

--- repl/repl.go
+++ repl/repl.go
@@ -6,9 +6,10 @@ import (
 	"io"
 	"monkey/lexer"
 	"monkey/parser"
+  "monkey/evaluator"
 )
 
-const PROMPT = ">>"
+const PROMPT = ">> "
 
 func Start(in io.Reader, out io.Writer) {
   scanner := bufio.NewScanner(in)
@@ -30,8 +31,11 @@ func Start(in io.Reader, out io.Writer) {
       continue
     }
 
-    io.WriteString(out, program.String())
-    io.WriteString(out, "\n")
+    evaluated := evaluator.Eval(program)
+    if evaluated != nil {
+      io.WriteString(out, evaluated.Inspect())
+      io.WriteString(out, "\n")
+    }
   }
 }
 
